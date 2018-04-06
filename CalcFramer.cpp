2042047b164cab766062a3684f3c9aa723bf75f4
范志康 <goodman.capu@gmail.com>
2018-04-06 15:55:22 -0700

    answer

--- CalcFramer.cpp
+++ CalcFramer.cpp
@@ -3,29 +3,45 @@
 
 using namespace std;
 
+static const string DELIMINATOR = "\r\n";
+
 void CalcFramer::append(string chars)
 {
 	// PUT YOUR CODE HERE
+	messageBuffer += chars;
+	while (true) {
+		size_t pos = messageBuffer.find_first_of(DELIMINATOR);
+		if (pos != string::npos) {
+			messages.push_back(messageBuffer.substr(0, pos));
+			messageBuffer.erase(0, pos + 2);
+		} else {
+			break;
+		}
+	}
 }
 
 bool CalcFramer::hasMessage() const
 {
 	// PUT YOUR CODE HERE
-	return false;
+	return !messages.empty();
 }
 
 string CalcFramer::topMessage() const
 {
 	// PUT YOUR CODE HERE
-	return "";
+	return messages.front();
 }
 
 void CalcFramer::popMessage()
 {
 	// PUT YOUR CODE HERE
+	messages.pop_front();
 }
 
 void CalcFramer::printToStream(ostream& stream) const
 {
 	// (OPTIONAL) PUT YOUR CODE HERE--useful for debugging
+	for (string message : messages) {
+		stream << message << '\n';
+	}
 }
--- CalcFramer.hpp
+++ CalcFramer.hpp
@@ -2,25 +2,30 @@
 #define CALCFRAMER_HPP
 
 #include <iostream>
+#include <queue>
+
+using namespace std;
 
 class CalcFramer {
 public:
-	void append(std::string chars);
+	void append(string chars);
 
 	// Does this buffer contain at least one complete message?
 	bool hasMessage() const;
 
 	// Copy the first instruction in this buffer into the provided class
-	std::string topMessage() const;
+	string topMessage() const;
 
 	// Pops off the first instruction from the buffer
 	void popMessage();
 
 	// prints the string to an ostream (useful for debugging)
-	void printToStream(std::ostream& stream) const;
+	void printToStream(ostream& stream) const;
 
 protected:
 	// PUT ANY FIELDS/STATE HERE
+	string messageBuffer;
+	deque<string> messages;
 };
 
 #endif // CALCFRAMER_HPP
